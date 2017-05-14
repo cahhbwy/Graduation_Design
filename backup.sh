5e006225e315126f922a4d50cdaaa487733dd61b
cahhbwy <cahhbwy@mail.ustc.edu.cn>
2017-05-14 09:54:37 +0800

    "2017-05-14_09:54:37"

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git add --all
-echo `date "+%Y-%m-%d %H:%M:%S"` > $t
+echo `date "+%Y-%m-%d_%H:%M:%S"`
+t=`date "+%Y-%m-%d_%H:%M:%S"`
 git commit -m "\""${t}"\""
 git push origin master
